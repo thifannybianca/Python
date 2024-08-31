65fbc39a24242d143d3c98575f55075a53f5bdd1
thifannybianca <biancathifanny7@gmail.com>
2024-08-31 17:21:04 -0300

    Update 01-aula01.py

--- 01-aula01.py
+++ 01-aula01.py
@@ -2,4 +2,19 @@
 # Continuar na linha de baixo
 print("Thifanny") # comentários a linha do código
 print('Thifanny')
-print('Thifanny Bianca')
+print('Thifanny "Bibi" Bianca')
+print("Thifanny 'Bibi' Bianca")
+print("Thifanny \"Bibi\" Bianca)
+
+teste = "oi"
+print(teste)
+print(type(teste))
+teste = 17
+print(teste)
+print(type(teste))
+teste = 12.1
+print(teste)
+print(type(teste))
+teste = false
+print(teste)
+print(type(teste))
