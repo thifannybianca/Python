728f38b6dd2bc3971c263d877c7a5266722e6a74
thifannybianca <biancathifanny7@gmail.com>
2024-09-11 14:45:44 -0300

    Add files via upload

---
+++ 02-aula02.py
@@ -0,0 +1,9 @@
+# atribuição de variáveis
+nome = "Thifanny"
+idade = 15
+altura = 1.68
+
+# exibir mensagem na tela
+print(" nome: ", nome, "idade: ",idade,"altura: ",altura,)
+# formatação com f-string
+print(f"nome: {nome} idade: {idade} altura: {altura}")
