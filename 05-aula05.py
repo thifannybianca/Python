ced1b8aebd08865e9e9250b76bed7bd93ddeddda
thifannybianca <biancathifanny7@gmail.com>
2024-09-11 17:39:02 -0300

    Add files via upload

---
+++ 05-aula05.py
@@ -0,0 +1,12 @@
+# algoritmo media: criar um algoritmo com 4 notas
+# meida final
+
+print("algoritmo do calculo da média")
+
+nota1 = float(input("digite a nota 1: "))
+nota2 = float(input("digite a nota 2: "))
+nota3 = float(input("digite a nota 3: "))
+nota4 = float(input("digite a nota 4: "))
+
+media = (nota1 + nota2 + nota3 + nota4) / 4
+print(f"a média final é {media:.2f}")#o .2f é usado para mostrar casas decimais
