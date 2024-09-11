0ff7bcd291745df5539e6225eda17fb46437098b
thifannybianca <biancathifanny7@gmail.com>
2024-09-11 17:14:46 -0300

    Add files via upload

---
+++ 04-operadores-aritmeticos.py
@@ -0,0 +1,24 @@
+# operadores aritmétricos
+valorA = float(input("digite o valor de A"))
+valorB = float(input("digite o valor de B"))
+# soma
+soma = valorA + valorB
+print(f"a soma = {soma}")
+# subtração
+subtração = valorA - valorB
+print(f"a subtração = {subtração}")
+# multiplicação
+multiplicação = valorA * valorB
+print(f"a multiplicação = {multiplicação}")
+# divisão
+divisão = valorA / valorB
+print(f"a divisão = {divisão}")
+# divisão inteira
+divisão_inteira = valorA // valorB
+print(f"a divisão inteira = {divisão_inteira}")
+# resto da divisão 
+resto_divisão = valorA % valorB
+print(f"o resto da divisão = {resto_divisão}")
+# exponencial
+exponenciacao = valorA ** valorB
+print(f"a exponenciação = {exponenciacao}")
