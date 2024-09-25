c1468e2e66e9dce336ca4acbe5563576b94dae1b
thifannybianca <biancathifanny7@gmail.com>
2024-09-25 15:11:19 -0300

    Add files via upload

---
+++ tabuada.py
@@ -0,0 +1,8 @@
+# variáveis
+numero = int(input("Digite um número para ver sua tabuada: "))
+contador = 1
+
+while contador <= 10:
+  resultado = numero * contador
+  print(f"{numero} x {contador} = {resultado}")
+  contador += 1
